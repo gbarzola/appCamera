73ea4b22bfc7a6abcaa0ad6ba2c06f679141923f
Gerardo <gbarzolas@pucp.pe>
2018-08-13 15:58:20 -0500

    configuracion para la habilitacion de la camara del celular desde mainActivity

--- app/src/main/java/com/example/gerardo/appcamera/MainActivity.java
+++ app/src/main/java/com/example/gerardo/appcamera/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.gerardo.appcamera;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,14 +10,13 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int REQUEST_TAKE_PHOTO = 0;
-    public static final int REQUEST_TAKE_VIDEO = 1;
-    public static final int REQUEST_PICK_PHOTO = 2;
+    public static final int REQUEST_TOMA_FOTO = 0;
+    public static final int REQUEST_TOMA_VIDEO = 1;
+    public static final int REQUEST_PICK_FOTO = 2;
     public static final int REQUEST_PICK_VIDEO = 3;
 
     public static final int MEDIA_TYPE_IMAGE = 4;
     public static final int MEDIA_TYPE_VIDEO = 5;
-
 
 
     @Override
@@ -26,7 +27,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.tomaFoto)
-    void tomaFoto(){}
+    void tomaFoto(){
+        Intent tomaFotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(tomaFotoIntent, REQUEST_TOMA_FOTO);
+    }
 
     @OnClick(R.id.tomaVideo)
     void tomaVideo(){}
