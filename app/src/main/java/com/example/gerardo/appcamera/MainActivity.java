86e3aab511a4ab374a328bb3d4f498effe3fc743
Gerardo <gbarzolas@pucp.pe>
2018-08-13 15:10:44 -0500

    inicializar butterknife y crear eventos onclick para cada icono

--- app/src/main/java/com/example/gerardo/appcamera/MainActivity.java
+++ app/src/main/java/com/example/gerardo/appcamera/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.gerardo.appcamera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_TAKE_PHOTO = 0;
@@ -19,8 +22,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.tomaFoto)
+    void tomaFoto(){}
 
+    @OnClick(R.id.tomaVideo)
+    void tomaVideo(){}
+
+    @OnClick(R.id.pickFoto)
+    void pickFoto(){}
+
+    @OnClick(R.id.pickVideo)
+    void pickVideo(){}
 
 }
